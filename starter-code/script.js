841f4ed001c0dab47977ab166f094a96aa6c667b
winsofttutorials <winsofttutorials@gmail.com>
2023-05-29 16:22:55 +0100

    gallery changer

--- starter-code/script.js
+++ starter-code/script.js
@@ -17,16 +17,9 @@ function openMenu() {
 // -----------------Destination tabinfo---------------
 var tabLinks = document.getElementsByClassName("activeTabLinks");
 var tabcontents = document.getElementsByClassName("tab-content");
-var images = document.getElementsByClassName("imagesPhoto");
+var images = document.getElementsByClassName("planetImages");
 
-function imagesGallery() {
-  for (const image of images) {
-    image.classList.remove("activeImages");
-  }
-  event.currentTarget.classList.add("activeImages");
-}
-
-function tabContentSlider(tabname) {
+function tabContentSlider(plantimg, tabname) {
   for (const tabLink of tabLinks) {
     tabLink.classList.remove("activeTabLink");
   }
@@ -34,7 +27,12 @@ function tabContentSlider(tabname) {
   for (const tabcontent of tabcontents) {
     tabcontent.classList.remove("activeTab");
   }
-  imagesGallery();
+
+  for (const image of images) {
+    image.classList.remove("activeImages");
+  }
+
   event.currentTarget.classList.add("activeTabLink");
   document.getElementById(tabname).classList.add("activeTab");
+  document.getElementById(plantimg).classList.add("activeImages");
 }
