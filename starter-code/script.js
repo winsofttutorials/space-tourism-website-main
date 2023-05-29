5646a5559314de524ced6b10d5a79a6fd20b413b
winsofttutorials <winsofttutorials@gmail.com>
2023-05-29 03:32:45 +0100

    gallery

--- starter-code/script.js
+++ starter-code/script.js
@@ -13,3 +13,28 @@ let menuBar = document.getElementById("sideMenu");
 function openMenu() {
   menuBar.classList.toggle("menuwidth");
 }
+
+// -----------------Destination tabinfo---------------
+var tabLinks = document.getElementsByClassName("activeTabLinks");
+var tabcontents = document.getElementsByClassName("tab-content");
+var images = document.getElementsByClassName("imagesPhote");
+
+function imagesGallery() {
+  for (const image of images) {
+    image.classList.remove("activeImages");
+  }
+  event.currentTarget.classList.add("activeImages");
+}
+
+function tabContentSlider(tabname) {
+  for (const tabLink of tabLinks) {
+    tabLink.classList.remove("activeTabLink");
+  }
+
+  for (const tabcontent of tabcontents) {
+    tabcontent.classList.remove("activeTab");
+  }
+  imagesGallery();
+  event.currentTarget.classList.add("activeTabLink");
+  document.getElementById(tabname).classList.add("activeTab");
+}
